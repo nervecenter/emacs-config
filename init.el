dfd4c5eef75c73bc5357b578480c20a4f9b16935
Chris Collazo <nervecenter7@gmail.com>
2016-10-01 21:23:49 -0400

    Switched to use-package.

--- init.el
+++ init.el
@@ -1,121 +1,123 @@
 ;;
-;; Packages and Archives
+;; Packages
 ;;
 
 (require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
 
-;; List packages to download
-(setq package-list
-      '(evil
-	evil-leader
-	evil-nerd-commenter
-	ido
-	ido-ubiquitous
-	ido-better-flex
-	ido-vertical-mode
-	smart-mode-line
-	smooth-scrolling
-	zenburn-theme
-	nlinum-relative
-	rainbow-delimiters
-	))
-
-;; Set the archive sources
-(setq package-archives
-      '(("elpa"         . "https://tromey.com/elpa/")
-	("gnu"          . "https://elpa.gnu.org/packages/")
-	("org"          . "https://orgmode.org/elpa/")
-	("marmalade"    . "https://marmalade-repo.org/packages/")
-	("melpa-stable" . "https://stable.melpa.org/packages/")
-	("melpa"        . "https://melpa.org/packages/")))
-
-;; Activate all the packages (in particular autoloads)
 (package-initialize)
 
-;; Make sure to have downloaded archive description.
-(or (file-exists-p package-user-dir)
-    (package-refresh-contents))
-
-;; Install missing packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 ;;
 ;; Emacs Settings
 ;;
 
-(set-face-attribute 'default t :font "DejaVu Sans Mono for Powerline")
+(set-face-attribute 'default t :font "DejaVu Sans Mono for Powerline-11")
 (load-theme 'zenburn t)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (show-paren-mode t)
 (hl-line-mode t)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
 
 ;;
 ;; Package Settings
 ;;
 
 ;; - RecentF -
-(require 'recentf)
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
+(use-package recentf
+  :ensure t
+  :init
+  (recentf-mode 1)
+  (setq recentf-max-menu-items 25))
 
 ;; - Ido -
-(require 'ido)
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-(ido-vertical-mode 1)
-(setq ido-vertical-define-keys 'C-n-C-p-up-and-down)
+(use-package ido
+  :config
+  (use-package ido-ubiquitous
+    :ensure t)
+  (use-package ido-better-flex
+    :ensure t)
+  (use-package ido-vertical-mode
+    :ensure t)
+  (setq ido-enable-flex-matching t
+        ido-everywhere t
+        ido-vertical-define-keys 'C-n-C-p-up-and-down)
+  (ido-mode 1)
+  (ido-vertical-mode 1)
 
-(defun ido-recentf-open ()
-  "Use `ido-completing-read' to find a recent file."
-  (interactive)
-  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
-      (message "Opening file...")
-    (message "Aborting")))
+  (defun ido-recentf-open ()
+    "Use `ido-completing-read' to find a recent file."
+    (interactive)
+    (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+        (message "Opening file...")
+      (message "Aborting"))))
 
 ;; - Evil -
 
 ;; evil-leader bindings
-(require 'evil-leader)
-(setq evil-leader/in-all-states 1)
-(evil-leader/set-leader "<SPC>")
-(evil-mode nil)
-(global-evil-leader-mode)
-(evil-mode 1)
-(kill-buffer "*Messages*")
+(use-package evil
+  :ensure t
+  :config
+  (use-package evil-leader
+    :ensure t
+    :config
+    (setq evil-leader/in-all-states 1)
+    (evil-leader/set-leader "<SPC>")
+    (evil-leader/set-key
+      "d" 'kill-buffer
+      "f" 'find-file
+      "b" 'ido-switch-buffer
+      "r" 'ido-recentf-open
+      "t" 'split-window-right
+      "w" 'delete-other-windows
+      "n" 'previous-buffer
+      "m" 'next-buffer
+    ))
+  (evil-mode nil)
+  (global-evil-leader-mode)
+  (evil-mode 1)
+  (kill-buffer "*Messages*"))
 
-(evil-leader/set-key
-  "d" 'kill-buffer
-  "f" 'find-file
-  "b" 'ido-switch-buffer
-  "r" 'ido-recentf-open
-  "t" 'split-window-right
-  "w" 'delete-other-windows
-  "n" 'previous-buffer
-  "m" 'next-buffer
-  )
 
 ;; - Smart Mode Line -
-(sml/setup)
+(use-package smart-mode-line
+  :ensure t
+  :init
+  (sml/setup))
 
 ;; - Rainbow Delimiters -
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
 ;; - nlinum -
-(require 'nlinum-relative)
-(nlinum-relative-setup-evil)
-(add-hook 'prog-mode-hook 'nlinum-relative-mode)
-(setq nlinum-relative-redisplay-delay 0)
-(setq nlinum-relative-current-symbol "")
-(setq nlinum-relative-offset 0)
+(use-package nlinum
+  :ensure t
+  :init
+  (nlinum-relative-setup-evil)
+  (add-hook 'prog-mode-hook 'nlinum-relative-mode)
+  :config
+  (setq nlinum-relative-redisplay-delay 0
+        nlinum-relative-current-symbol "")
+        nlinum-relative-offset 0)
 
 ;; - Smooth Scrolling -
-(setq scroll-margin 5
-      scroll-conservatively 9999
-      scroll-step 1)
+(use-package smooth-scrolling
+  :ensure t
+  :init
+  (setq scroll-margin 5
+        scroll-conservatively 9999
+        scroll-step 1))
 
 ;;
 ;; Keybinds
