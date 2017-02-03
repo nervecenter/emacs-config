08718134d8089b58105c51307e02201ffc9fe002
Chris Collazo <nervecenter7@gmail.com>
2017-02-03 06:50:55 -0500

    Turned off smooth scroll on Mac.

--- init.el
+++ init.el
@@ -203,16 +203,17 @@
 ;(global-set-key (kbd "\C-m") 'next-buffer)
 ;(global-set-key (kbd "\C-p") 'ido-recentf-open)
 
-;; - Emacs-w64 -
+;; - Emacs-w64 and Emacs Mac -
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mac-mouse-wheel-smooth-scroll nil)
  '(package-selected-packages
    (quote
-    (rainbow-delimiters relative-line-numbers zenburn-theme smooth-scrolling smart-mode-line ido-vertical-mode ido-better-flex ido-ubiquitous evil-nerd-commenter evil-leader evil))))
+	(rainbow-delimiters relative-line-numbers zenburn-theme smooth-scrolling smart-mode-line ido-vertical-mode ido-better-flex ido-ubiquitous evil-nerd-commenter evil-leader evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
